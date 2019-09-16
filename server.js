6896a971f99078ba82c3696f70a5439a2c9a9a7e
tania <tanislavens@yahoo.com>
2019-09-16 14:26:20 -0400

    installed dependencies and set up files

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+
+const usersRouter = require('./users/users-router');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.use('/api/users', usersRouter);
+
+module.exports = server;
