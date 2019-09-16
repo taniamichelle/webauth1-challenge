f4224f3ca94bb252daa097392c1eaa325fbaae6e
tania <tanislavens@yahoo.com>
2019-09-16 14:32:28 -0400

    added migration and middleware

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-const bcrypt = require('bcryptjs');
 
 const usersRouter = require('./users/users-router');
 
---
+++ users/users-router.js
@@ -0,0 +1,61 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+const Users = require('./users/users-model.js');
+const restricted = require('./auth/restricted-middleware.js');
+
+server.get('/', (req, res) => {
+    res.send("It's alive!");
+});
+
+server.post('/api/register', (req, res) => {
+    let { username, password } = req.body;
+
+    const hash = bcrypt.hashSync(password, 8); // it's 2 ^ 8, not 8 rounds
+
+    Users.add({ username, password: hash })
+        .then(saved => {
+            res.status(201).json(saved);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+server.post('/api/login', (req, res) => {
+    let { username, password } = req.body;
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                res.status(200).json({ message: `Welcome ${user.username}!` });
+            } else {
+                res.status(401).json({ message: 'You cannot pass!' });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+server.get('/api/users', restricted, (req, res) => {
+    Users.find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => res.send(err));
+});
+
+server.get('/hash', (req, res) => {
+    const name = req.query.name;
+
+    // hash the name
+    const hash = bcrypt.hashSync(name, 8); // use bcryptjs to hash the name
+    res.send(`the hash for ${name} is ${hash}`);
+});
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+
+module.exports = router;
